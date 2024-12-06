aae242d3aa89c5446431e37c3f3ae858efe1e5e3
fqhkhwrzm <muhammadfaqihk16@gmail.com>
2024-12-06 17:07:56 +0700

    mengubah host dan edit gitignore

--- src/server/server.js
+++ src/server/server.js
@@ -9,7 +9,7 @@ const InputError = require('../exceptions/InputError');
 (async () => {
     const server = Hapi.server({
         port: 3000,
-        host: 'localhost',
+        host: '0.0.0.0',
         routes: {
             // Pakai cors untuk mengizinkan permintaan dari semua origin untuk dapat mengakses sumber daya dalam server
             cors: {
